7df7eaf2e6437fc720507b9ba0a1e9beea1c7b52
Devin W. Leaman <git@4lch4.email>
2023-07-22 19:29:38 -0500

    refactor: replaced default api port to 8080

--- src/lib/Config.ts
+++ src/lib/Config.ts
@@ -4,7 +4,7 @@ export function getAppConfig(): IAppConfig {
   return {
     apiName: process.env.API_NAME || 'k-blight-api',
     apiVersion: process.env.API_VERSION || '0.0.1',
-    apiPort: parseInt(process.env.API_PORT || '8000'),
+    apiPort: parseInt(process.env.API_PORT || '8080'),
     apiPrefix: process.env.API_PREFIX || '/api/v1',
   }
 }
